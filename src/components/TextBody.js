48933aefb05d33ae52fe737e79aba68d0b8f40fa
davidgeorgebell <bell.dave93@gmail.com>
2019-08-10 13:07:59 +0800

    Dark Theme toggle using React Context

--- src/components/TextBody.js
+++ src/components/TextBody.js
@@ -1,58 +1,35 @@
 import React from 'react';
-import { ThemeProvider } from './contexts/theme'
-import Nav from './components/Nav';
-import './index.css'
+import { ThemeConsumer } from '../contexts/theme'
 
-class App extends React.Component {
-  constructor(props) {
-    super(props)
+function TextBody () {
+        return(
+            <ThemeConsumer>
+            {({ theme }) => (
+                <div className='TextBody'>
+                <h1>Bacon Ipsum</h1>
+                <p>
+                Bacon ipsum dolor amet filet mignon hamburger pig, lorem pork belly veniam flank salami nisi ea fatback bresaola elit. Short ribs nisi leberkas short loin cow ex. Prosciutto short ribs ham, ribeye excepteur magna frankfurter enim laboris aliquip. Meatloaf lorem turkey, labore dolore tri-tip non alcatra ullamco boudin aliqua eu capicola. Quis bacon landjaeger tempor, aliqua frankfurter magna kielbasa officia incididunt laboris. Shank officia aliquip jowl lorem eiusmod dolor est nostrud excepteur.
+                </p>
+                <p>
+                Bacon ipsum dolor amet filet mignon hamburger pig, lorem pork belly veniam flank salami nisi ea fatback bresaola elit. Short ribs nisi leberkas short loin cow ex. Prosciutto short ribs ham, ribeye excepteur magna frankfurter enim laboris aliquip. Meatloaf lorem turkey, labore dolore tri-tip non alcatra ullamco boudin aliqua eu capicola. Quis bacon landjaeger tempor, aliqua frankfurter magna kielbasa officia incididunt laboris. Shank officia aliquip jowl lorem eiusmod dolor est nostrud excepteur.
+                </p>
+                <p>
+                Bacon ipsum dolor amet filet mignon hamburger pig, lorem pork belly veniam flank salami nisi ea fatback bresaola elit. Short ribs nisi leberkas short loin cow ex. Prosciutto short ribs ham, ribeye excepteur magna frankfurter enim laboris aliquip. Meatloaf lorem turkey, labore dolore tri-tip non alcatra ullamco boudin aliqua eu capicola. Quis bacon landjaeger tempor, aliqua frankfurter magna kielbasa officia incididunt laboris. Shank officia aliquip jowl lorem eiusmod dolor est nostrud excepteur.
+                </p>
+                <p>
+                Bacon ipsum dolor amet filet mignon hamburger pig, lorem pork belly veniam flank salami nisi ea fatback bresaola elit. Short ribs nisi leberkas short loin cow ex. Prosciutto short ribs ham, ribeye excepteur magna frankfurter enim laboris aliquip. Meatloaf lorem turkey, labore dolore tri-tip non alcatra ullamco boudin aliqua eu capicola. Quis bacon landjaeger tempor, aliqua frankfurter magna kielbasa officia incididunt laboris. Shank officia aliquip jowl lorem eiusmod dolor est nostrud excepteur.
+                </p>
+                
+                <p>
+                Bacon ipsum dolor amet filet mignon hamburger pig, lorem pork belly veniam flank salami nisi ea fatback bresaola elit. Short ribs nisi leberkas short loin cow ex. Prosciutto short ribs ham, ribeye excepteur magna frankfurter enim laboris aliquip. Meatloaf lorem turkey, labore dolore tri-tip non alcatra ullamco boudin aliqua eu capicola. Quis bacon landjaeger tempor, aliqua frankfurter magna kielbasa officia incididunt laboris. Shank officia aliquip jowl lorem eiusmod dolor est nostrud excepteur.
+                </p>
+                
 
-    this.state = {
-        theme: 'light',
-        toggleTheme: () => {
-          this.setState(({ theme }) => ({
-            theme: theme === 'light' ? 'dark' : 'light'
-          }))
-        }
+            </div>
+            )}
+            
+            </ThemeConsumer>
+        )
     }
-  }
-  render(){
-  return (
-    <ThemeProvider value={this.state}>
-    <div className={this.state.theme}>
-    <div className="container">
-    <Nav />
-     <h1>Bacon Ipsum</h1>
-     <p>
-                Bacon ipsum dolor amet filet mignon hamburger pig, lorem pork belly veniam flank salami nisi ea fatback bresaola elit. Short ribs nisi leberkas short loin cow ex. Prosciutto short ribs ham, ribeye excepteur magna frankfurter enim laboris aliquip. Meatloaf lorem turkey, labore dolore tri-tip non alcatra ullamco boudin aliqua eu capicola. Quis bacon landjaeger tempor, aliqua frankfurter magna kielbasa officia incididunt laboris. Shank officia aliquip jowl lorem eiusmod dolor est nostrud excepteur.
-     </p>
-     <p>
-                Bacon ipsum dolor amet filet mignon hamburger pig, lorem pork belly veniam flank salami nisi ea fatback bresaola elit. Short ribs nisi leberkas short loin cow ex. Prosciutto short ribs ham, ribeye excepteur magna frankfurter enim laboris aliquip. Meatloaf lorem turkey, labore dolore tri-tip non alcatra ullamco boudin aliqua eu capicola. Quis bacon landjaeger tempor, aliqua frankfurter magna kielbasa officia incididunt laboris. Shank officia aliquip jowl lorem eiusmod dolor est nostrud excepteur.
-                </p>
-                
-                <p>
-                Bacon ipsum dolor amet filet mignon hamburger pig, lorem pork belly veniam flank salami nisi ea fatback bresaola elit. Short ribs nisi leberkas short loin cow ex. Prosciutto short ribs ham, ribeye excepteur magna frankfurter enim laboris aliquip. Meatloaf lorem turkey, labore dolore tri-tip non alcatra ullamco boudin aliqua eu capicola. Quis bacon landjaeger tempor, aliqua frankfurter magna kielbasa officia incididunt laboris. Shank officia aliquip jowl lorem eiusmod dolor est nostrud excepteur.
-                </p>
-                
-                <p>
-                Bacon ipsum dolor amet filet mignon hamburger pig, lorem pork belly veniam flank salami nisi ea fatback bresaola elit. Short ribs nisi leberkas short loin cow ex. Prosciutto short ribs ham, ribeye excepteur magna frankfurter enim laboris aliquip. Meatloaf lorem turkey, labore dolore tri-tip non alcatra ullamco boudin aliqua eu capicola. Quis bacon landjaeger tempor, aliqua frankfurter magna kielbasa officia incididunt laboris. Shank officia aliquip jowl lorem eiusmod dolor est nostrud excepteur.
-                </p>
-                
-                <p>
-                Bacon ipsum dolor amet filet mignon hamburger pig, lorem pork belly veniam flank salami nisi ea fatback bresaola elit. Short ribs nisi leberkas short loin cow ex. Prosciutto short ribs ham, ribeye excepteur magna frankfurter enim laboris aliquip. Meatloaf lorem turkey, labore dolore tri-tip non alcatra ullamco boudin aliqua eu capicola. Quis bacon landjaeger tempor, aliqua frankfurter magna kielbasa officia incididunt laboris. Shank officia aliquip jowl lorem eiusmod dolor est nostrud excepteur.
-                </p>
-                <p>
-                Bacon ipsum dolor amet filet mignon hamburger pig, lorem pork belly veniam flank salami nisi ea fatback bresaola elit. Short ribs nisi leberkas short loin cow ex. Prosciutto short ribs ham, ribeye excepteur magna frankfurter enim laboris aliquip. Meatloaf lorem turkey, labore dolore tri-tip non alcatra ullamco boudin aliqua eu capicola. Quis bacon landjaeger tempor, aliqua frankfurter magna kielbasa officia incididunt laboris. Shank officia aliquip jowl lorem eiusmod dolor est nostrud excepteur.
-                </p>
-                
-    </div>
-    </div>
-    
-    </ThemeProvider>
-    
-  );
-}
-}
 
-
-export default App;
+export default TextBody;
